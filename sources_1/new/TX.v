713ab4f3e3546a7c9203ee506821c92f2d961d99
YuriYZX <yzx_ustc@mail.ustc.edu.cn>
2023-03-17 20:59:01 +0800

    TX code with module RDY

--- sources_1/new/TX.v
+++ sources_1/new/TX.v
@@ -26,58 +26,43 @@ module uart_tx(
     input [7:0] d_tx,
     input vld_tx,
     output rdy_tx,
-    output txd
+    output reg txd
 );
 
 parameter TICKS_PER_BIT = 104; // assuming 100 MHz clock frequency
-
-reg [7:0] shift_reg;
-reg [3:0] bit_cnt;
-reg [TICKS_PER_BIT-1:0] tick_cnt; 
-reg start_bit, stop_bit;
-
-assign rdy_tx = (bit_cnt == 4'd0) && !vld_tx; // ready when no data is being transmitted or received
-assign txd = (bit_cnt == 4'd0) ? 1'b1 : shift_reg[0]; // idle high, send LSB first
-
+reg [8:0] SOR;
+reg [3:0] CNT;
+RDY rdy(
+    .vld_tx(vld_tx),
+    .rst(rst),
+    .clk(clk_tx),
+    .CNT(CNT),
+    .rdy_tx(rdy_tx)
+);
+always @(posedge clk_tx or posedge rst) begin
+    if(rst)begin
+        CNT<=0;
+        SOR<=9'b1111_11111;
+    end else begin
+        if(CNT)begin
+            CNT<=CNT-1;
+        end else if(rdy_tx) begin
+            CNT<=4'b1000;
+        end
+    end
+end
 always @(posedge clk_tx) begin
-    if (rst) begin // rst logic
-        bit_cnt <= 4'd0;
-        tick_cnt <= TICKS_PER_BIT - 1;
-        start_bit <= 1'b0;
-        stop_bit <= 1'b1;
-        shift_reg <= 8'h00;
-    end else begin // transmission logic
-        if (bit_cnt == 4'd0) begin // no data is being transmitted
-            if (vld_tx) begin // new data is available
-                bit_cnt <= 4'd9; // set bit count to 9 (start + data + stop)
-                tick_cnt <= TICKS_PER_BIT - 1; // set tick count to max value
-                start_bit <= 1'b0; // set start bit to low
-                stop_bit <= 1'b1; // set stop bit to high
-                shift_reg <= d_tx; // load data into shift register
-            end else begin // no new data is available 
-                tick_cnt <= TICKS_PER_BIT - 1; // keep tick count at max value 
-            end 
-        end else begin // some data is being transmitted 
-            if (tick_cnt == TICKS_PER_BIT - 1) begin // one bit period has elapsed 
-                tick_cnt <= TICKS_PER_BIT -2 ; // rst tick count 
-                case (bit_cnt) 
-                    4'd9: shift_reg[7:0] <= {shift_reg[6:0], start_bit}; // send start bit 
-                    4'd8: shift_reg[7:0] <= {shift_reg[6:0], d_tx[7]};   // send MSB of data 
-                    4'd7: shift_reg[7:0] <= {shift_reg[6:0], d_tx[6]};
-                    4'd6: shift_reg[7:0] <= {shift_reg[6:0], d_tx[5]};
-                    4'd5: shift_reg[7:0] <= {shift_reg[6:0], d_tx[4]};
-                    4'd4: shift_reg[7:0] <= {shift_reg[6:0], d_tx[3]};
-                    4'd3: shift_reg[7:0] <= {shift_reg[6:0], d_tx[2]};
-                    4'd2: shift_reg[7:0] <= {shift_reg[6:0], d_tx[1]};
-                    4'd1: shift_reg[7:0] <= {shift_reg[6:0], d_tx[0]};
-                    default: shift_reg[7 :0 ] <={shift_reg [6:0], stop_bit }; //send stop bit 
-                endcase 
-                bit_cnt <= bit_cnt - 1 ; //decrement bit count by one 
-            end else begin //one bit period has not elapsed yet 
-                tick_cnt <= tick_cnt - 1 ; //decrement tick count by one 
-            end  
-        end  
-    end  
-end  
-
+    if(rdy_tx&&vld_tx)begin
+        SOR<={d_tx[7:0],1'b0};
+        txd<=1'b0;
+    end
+    else if(CNT)begin
+        SOR<={1'b1,SOR[8:1]};
+        txd<=SOR[1];
+    end
+    else begin
+        SOR<=9'b1111_11111;
+        txd<=1'b1;
+    end
+end
 endmodule 
