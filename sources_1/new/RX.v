b05c68c681f332e10af5e5cb1ba35acd3de5eb79
YuriYZX <yzx_ustc@mail.ustc.edu.cn>
2023-03-17 23:48:28 +0800

    add frequency dividor to satisfy 9600 bitrate

--- sources_1/new/RX.v
+++ sources_1/new/RX.v
@@ -9,7 +9,8 @@ module uart_rx(
     output reg vld_rx   //1提示数据已经接收,等待其他模块拿走
 );
 
-parameter TICKS_PER_BIT = 104; // assuming 100 MHz clock frequency
+parameter TICKS_PER_BIT = 104>>4; // assuming 100 MHz clock frequency
+reg[3:0] fr_div;//分频器
 reg [7:0] CNT;
 reg [3:0] CNTb;
 wire process;//表示是否正在接收数据，1表示正在接受
@@ -22,6 +23,7 @@ CNTc cntc(
     output reg process
     */
     .clk(clk),
+    .fr_div(fr_div),
     .rst(rst),
     .rxd(rxd),
     .vld_rx(vld_rx),//vld_rx=1--->process=0
@@ -29,8 +31,17 @@ CNTc cntc(
 );
 always @(posedge clk or posedge rst) begin
     if(rst)begin
+        fr_div<=0;
+    end else if(fr_div==TICKS_PER_BIT) begin
+        fr_div<=0;
+    end else begin
+        fr_div<=fr_div+1;
+    end
+end
+always @(posedge clk or posedge rst) begin
+    if(rst)begin
         d_rx<=0;
-    end else if(process) begin
+    end else if(process&&fr_div==0) begin
         if(CNT==16)begin//取样中点
             CNT<=0;
             d_rx<={rxd,d_rx[7:1]};
@@ -44,14 +55,15 @@ always @(posedge clk or posedge rst) begin
         CNTb<=0;
         CNT<=0;
         vld_rx<=0;
+    end else if (fr_div==0) begin
+        else if(CNT==16)begin//取样中数据还未被接受
+            CNTb<=CNTb+1;
+            vld_rx<=0;
+        end
+        else if(CNTb==8)begin
+            vld_rx=1;
+            CNTb<=0;
+        end 
     end
-    else if(CNT==16)begin//取样中数据还未被接受
-        CNTb<=CNTb+1;
-        vld_rx<=0;
-    end
-    else if(CNTb==8)begin
-        vld_rx=1;
-        CNTb<=0;
-    end 
 end
 endmodule   
