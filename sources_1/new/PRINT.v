97e9a9f9fe4ed4fb42725d19ac1527fab81552fd
JYJSXX <JYJSXX@mail.ustc.edu.cn>
2023-03-18 20:58:42 +0800

    modified:   sources_1/new/PRINT.v

--- sources_1/new/PRINT.v
+++ sources_1/new/PRINT.v
@@ -1,4 +1,5 @@
 module PRINT(
+    input clk, rst,
     input [31:0] dout_tx,
     input type_tx, //0 stand for Byte, 1 stand for Word
     input req_tx, 
@@ -7,38 +8,73 @@ module PRINT(
     output reg [7:0] d_tx,
     output reg ack_tx
 );
-    reg [2:0] count; //一个字是四个字节，计数
-always @(*) begin
-    if(req_tx) begin
-        if(type_tx==0) begin
-            if(rdy_tx)
-            d_tx=dout_tx[7:0];
-            ack_tx=1;
+    reg [1:0] count; //count for how many Bytes left to send
+    wire req_tx_ps;
+
+    parameter [1:0] PRINT_INIT = 0,
+                    PRINT_BYTE = 1,
+                    PRINT_WORD = 2,
+                    PRINT_WAIT = 3;
+    reg Print_State = PRINT_INIT;
+    Posedge_Selector(
+        .clk(clk), .rstn(rst), .in(req_tx),
+        .out(req_tx_ps)
+    );
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) Print_State <= PRINT_INIT;
+        else begin
+            case (Print_State)
+                PRINT_INIT: begin
+                    if (req_tx_ps) 
+                        if (~type_tx) Print_State <= PRINT_BYTE;
+                        else Print_State <= PRINT_WORD;
+                end
+                PRINT_BYTE: begin
+                    Print_State <= PRINT_WAIT;
+                end
+                PRINT_WORD: begin
+                    Print_State <= PRINT_WAIT;
+                end
+                PRINT_WAIT: begin
+                    if (rdy_tx && ~|count) Print_State <= PRINT_INIT;
+                end
+            endcase
         end
-        else if(type_tx==1) begin
-            if(rdy_tx) begin
-                if(count==0) begin
-                    d_tx=dout_tx[7:0];///
-                    count=count+1;
-                end
-                else if(count==1) begin
-                    d_tx=dout_tx[15:8];
-                    count=count+1;
-                end
-                else if(count==2) begin
-                    d_tx=dout_tx[23:16];
-                    count=count+1;
-                end
-                else if(count==3) begin
-                    d_tx=dout_tx[31:24];
-                    count=0;
-                    ack_tx=1;
-                end
+    end
+
+always @(posedge clk) begin
+    case (Print_State) 
+        PRINT_INIT: begin
+            vld_tx <= 0;
+            d_tx <= 0;
+            ack_tx <= 0;
+            count <= 0;
+        end
+        PRINT_BYTE: begin
+            vld_tx <= 1;
+            d_tx <= dout_tx[7:0];
+            ack_tx <= 0;
+            count <= 0;
+        end
+        PRINT_WORD: begin
+            vld_tx <= 1;
+            ack_tx <= 0;
+            count <= count - 1;
+            case (count)
+                0: d_tx <= dout_tx[31:24];
+                3: d_tx <= dout_tx[23:16];
+                2: d_tx <= dout_tx[15:8];
+                1: d_tx <= dout_tx[7:0];
+            endcase
+        end
+        PRINT_WAIT: begin
+            if (rdy_tx) begin
+                vld_tx <= 1;
+                if (count == 0) ack_tx <= 1;
             end
         end
-        
-    end
-    
+    endcase
 end
     
 endmodule
